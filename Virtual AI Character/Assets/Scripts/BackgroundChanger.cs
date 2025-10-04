f9d7635d13973dccf4555f3c4d15669bfeca0814
sophia-tyy <tyysa0128@gmail.com>
2025-10-04 23:02:01 +0800

    added text input function

---
+++ Virtual AI Character/Assets/Scripts/BackgroundChanger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundChanger : MonoBehaviour
+{
+    [Header("Private Variable")]
+    [SerializeField] private static int backgroundCount = 4;
+    [SerializeField] private GameObject[] backgroundObjects = new GameObject[backgroundCount];
+    [SerializeField] private int currentBackgroundIndex = 0;
+
+    public void ChangeBackgroundImage()
+    {
+        int nextBackgroundIndex = (currentBackgroundIndex + 1) % backgroundCount;
+        backgroundObjects[currentBackgroundIndex].SetActive(false);
+        backgroundObjects[nextBackgroundIndex].SetActive(true);
+        currentBackgroundIndex = nextBackgroundIndex;
+    }
+}
